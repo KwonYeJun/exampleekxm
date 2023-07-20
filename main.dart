eb627006dac0d32820fa40be3fbbd9a467e520f8
gwonyejun <kyjuni1059@gmail.com>
2023-07-20 19:18:43 +0900

    null safety를 사용하여 어떤 변수나 데이터가 null값을 가질수도 있다 명시해준다

--- main.dart
+++ main.dart
@@ -1,11 +1,17 @@
 main() {
-  print('hello world');
-  var name = '주늬';
-  name = '권예준';
-  String nickname = '주늬?';
-  var testDynamic;
+  // print('hello world');
+  // var name = '주늬';
+  // name = '권예준';
+  // String nickname = '주늬?';
+  // var testDynamic;
 
-  if (testDynamic is String) {
-    testDynamic.
+  // if (testDynamic is String) {
+  //   testDynamic.
+  // }
+
+  String? name = 'kyj';
+  name = null;
+  if (name != null) {
+    name.isNotEmpty;
   }
 }
